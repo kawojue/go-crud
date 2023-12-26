1e7b81cc540e27f4078c82625477e6cb9dda945b
Raheem Kawojue <kawojue08@gmail.com>
2023-12-26 02:53:51 +0100

    [setup] main

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	db "github.com/kawojue/go-crud/DB"
+	"github.com/kawojue/go-crud/utils"
+)
+
+func init() {
+	gin.SetMode(gin.ReleaseMode)
+
+	utils.LoadEnv(nil)
+	db.ConnectDB()
+}
+
+func main() {
+	router := gin.Default()
+	PORT := utils.GetEnv("PORT", "2002")
+
+	router.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+	}))
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Welcome",
+		})
+	})
+
+	router.Run(fmt.Sprintf(":%s", PORT))
+}
